dc6d9001c044168cd3cf0bf358c26063cfef0160
maphanixx <81342646+maphanixx@users.noreply.github.com>
2022-11-03 20:26:53 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,105 @@
+// reloj
+
+var fechaHora = new Date();
+var Horas = fechaHora.getHours();
+var Minutos = fechaHora.getMinutes();
+var Segundos = fechaHora.getSeconds();
+
+var sufijo = ' am';
+if(Horas > 12) {
+  Horas = Horas - 12;
+  sufijo = ' pm';
+}
+
+if(Horas < 10) { Horas = '0' + Horas; }
+if(Minutos < 10) { Minutos = '0' + Minutos; }
+if(Segundos < 10) { Segundos = '0' + Segundos; }
+
+document.getElementById("reloj").innerHTML = Horas+':'+Minutos+':'+Segundos+sufijo;
+
+//barra lateral
+
+const btnToggle = document.querySelector('.toggle-btn');
+
+btnToggle.addEventListener('click', function () {
+  console.log('clik')
+  document.getElementById('sidebar').classList.toggle('active');
+  console.log(document.getElementById('sidebar'))
+});
+
+//temporizador
+
+let horas = 7;
+let minutos = 0;
+let segundos = 0;
+cargarSegundo();
+
+//Definimos y ejecutamos los segundos
+function cargarSegundo(){
+    let txtSegundos;
+
+    if(segundos < 0){
+        segundos = 59;
+    }
+
+    //Mostrar Segundos en pantalla
+    if(segundos < 10){
+        txtSegundos = `0${segundos}`;
+    }else{
+        txtSegundos = segundos;
+    }
+    document.getElementById('segundos').innerHTML = txtSegundos;
+    segundos--;
+
+    cargarMinutos(segundos);
+}
+
+//Definimos y ejecutamos los minutos
+function cargarMinutos(segundos){
+    let txtMinutos;
+
+    if(segundos == -1 && minutos !== 0){
+        setTimeout(() =>{
+            minutos--;
+        },500)
+    }else if(segundos == -1 && minutos == 0){
+        setTimeout(() =>{
+            minutos = 59;
+        },500)
+    }
+
+    //Mostrar Minutos en pantalla
+    if(minutos < 10){
+        txtMinutos = `0${minutos}`;
+    }else{
+        txtMinutos = minutos;
+    }
+    document.getElementById('minutos').innerHTML = txtMinutos;
+    cargarHoras(segundos,minutos);
+}
+
+//Definimos y ejecutamos las horas
+function cargarHoras(segundos,minutos){
+    let txtHoras;
+
+    if(segundos == -1 && minutos == 0 && horas !== 0){
+        setTimeout(() =>{
+            horas--;
+        },500)
+    }else if(segundos == -1 && minutos == 0 && horas == 0){
+        setTimeout(() =>{
+            horas = 2;
+        },500)
+    }
+
+    //Mostrar Horas en pantalla
+    if(horas < 10){
+        txtHoras = `0${horas}`;
+    }else{
+        txtHoras = horas;
+    }
+    document.getElementById('horas').innerHTML = txtHoras;
+}
+
+//Ejecutamos cada segundo
+setInterval(cargarSegundo,1000);
